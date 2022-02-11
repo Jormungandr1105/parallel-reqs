f86b52a00a2d5307597c7668a3661702762dffd5
jormungandr1105 <maxtmarshall99@gmail.com>
2022-02-11 03:03:36 -0500

    RUNNING

--- src/job_manager/job.py
+++ src/job_manager/job.py
@@ -1,24 +1,30 @@
 # Struct that Stores Job Data
+import os
 import time
 import math
+import threading
+import subprocess
 
 class Job():
 	### CLASS VARIABLES
 
 
 	### INIT
-	def __init__(self, id, start_time, end_time, thread, data):
+	def __init__(self, id, start_time, end_time, data):
 		# id is an hex code assigned to the job
 		# start_time is a float of time in seconds since the Epoch
 		self.id = id
 		self.start_time = start_time
 		self.end_time = end_time
-		self.thread = thread
-		self.name = data[0]
-		self.file = data[1]
-		self.hostfile = data[3]
-		self.num_nodes = data[4]
+		self.thread = None
+		self.path = data[0]
+		self.name = data[1]
+		self.file = data[2]
+		self.hostfile = data[4]
+		self.num_nodes = data[5]
 		self.percent_complete = 0
+		self.finished = False
+		self.process = None
 
 	### GETTERS
 	def get_id(self):
@@ -53,6 +59,38 @@ class Job():
 	def set_pipes(self, in_pipe, out_pipe):
 		self.in_pipe = in_pipe
 		self.out_pipe = out_pipe
+
+	### RUN
+	def run(self):
+		exec_file = os.path.join(self.path,self.file)
+		execute = "mpiexec -n {0} --hostfile {1} ".format(self.num_nodes, os.path.join(self.path,self.hostfile))
+		try:
+			ext_begin = exec_file.rfind(".")
+			if exec_file[ext_begin:] == ".py":
+				execute += "python3 -m mpi4py {0}".format(exec_file)
+			else:
+				execute += "{0}".format(exec_file)
+		except ValueError:
+			execute += "{0}".format(exec_file)
+		finally:
+			args = execute.split(" ")
+			#print(args)
+			with subprocess.Popen(args, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as job:
+				self.process = job
+				for line in job.stdout:
+					print(line)
+
+		if job.returncode == 0:
+			print("PROCESS COMPLETED SUCCESSFULLY")
+			self.finished=True
+		elif job.returncode == 1:
+			print("SYSTEM ENCOUNTERED AN ERROR")
+		return
+
+	def kill(self):
+		if not self.finished:
+			self.process.kill()
+			self.finished=True
 
 
 if __name__ == "__main__":
